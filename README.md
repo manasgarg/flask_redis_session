b76403599dd2c8c191465fd6c283605d1d95cf78
Manas Garg <manasgarg@gmail.com>
2013-01-09 16:04:45 +0530

    Initial Commit

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ flask_redis_session
 ===================
 
 Flask Redis Session Store. Copied from http://flask.pocoo.org/snippets/75/. Available for dropping into projects.
+
+Usage:
+======
+
+from flask_redis_session import RedisSessionInterface
+app.session_interface = RedisSessionInterface()
---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup( name="flask_redis_session",
+    version = "0.1",
+    description = "Flask session interface to use redis as session backend. ",
+    author = "Manas Garg",
+    author_email = "manasgarg@gmail.com",
+    license = "BSD License",
+    url = "https://github.com/manasgarg/flask_redis_session",
+    py_modules = ["flask_redis_session"],
+    long_description = ""
+)
+
